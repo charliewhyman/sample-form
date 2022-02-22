07675f1b49cf4396d1012cd27074654cfa2be34f
cwhyman <cwhyma@gmail.com>
2022-02-22 09:52:52 -0500

    Added js form validation and country dropdown

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const email = document.getElementById('emailInput');
+const zipCode = document.getElementById('zipCodeInput');
+
+email.addEventListener('input', function (event) {
+  if (email.validity.typeMismatch) {
+    email.setCustomValidity('Please enter a valid email address');
+    email.reportValidity();
+  } else {
+    email.setCustomValidity('');
+  }
+});
+
+zipCode.addEventListener('input', function (event) {
+  if (zipCode.validity.patternMismatch) {
+    zipCode.setCustomValidity('Please enter a valid zip code');
+    zipCode.reportValidity();
+  } else {
+    zipCode.setCustomValidity('');
+  }
+});
